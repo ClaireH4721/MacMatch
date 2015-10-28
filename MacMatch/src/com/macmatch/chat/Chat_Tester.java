0b94d83d5131cae3d81546d34776a0cde345e319
Podd77 <podd777@live.co.uk>
2015-10-28 22:33:50 +0000

    Chat

---
+++ MacMatch/src/com/macmatch/chat/Chat_Tester.java
@@ -0,0 +1,15 @@
+package com.macmatch.chat;
+
+import com.macmatch.chat.ChatHome;
+
+public class Chat_Tester 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		ChatHome i = new ChatHome();
+		
+	}
+
+}
